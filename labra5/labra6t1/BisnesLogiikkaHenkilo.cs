76536594d24c761d538ab75dbe244abd7d9d3f6a
K8980 <K8980@ICT-421-WS-18>
2017-01-25 14:48:35 +0200

    Hetu loppu puuttuu

---
+++ labra5/labra6t1/BisnesLogiikkaHenkilo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    /// <summary>
+    /// This contains person properties
+    /// </summary>
+    class Henkilo
+    {
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string HeTu { get; set; }
+        public override string ToString()
+        {
+            return Etunimi + " " + Sukunimi + " " + HeTu;
+        }
+    }
+    ///This class holds person information in a collection
+    ///
+    class Henkilot
+    {
+        private List<Henkilo> henkilot;
+        public List<Henkilo> HenkiloLista
+        {
+            get { return henkilot; }
+        }
+        public Henkilot()
+        {
+            henkilot = new List<Henkilo>();
+        }
+        public void LisaaHenkilo(Henkilo hlo)
+        {
+            henkilot.Add(hlo);
+        }
+        public Henkilo HaeHenkilo(int index)
+        {
+            if (index < henkilot.Count)
+            {
+                return henkilot.ElementAt(index);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public Henkilo HaeHenkiloHeTulla(string hetu)
+        {
+            foreach (Henkilo hlo in henkilot)
+            {
+                if (hlo.HeTu == hetu)
+                {
+                    return hlo;
+                }
+            }
+            return null;
+        }
+    }
+
+  
+}
---
+++ labra5/labra6t1/Program.cs
@@ -0,0 +1,36 @@
+﻿using JAMK.IT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labra6t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaaHenkiloRekisteri();
+        }
+        static void TestaaHenkiloRekisteri()
+        {
+            //luodaan muuttuja
+            Henkilot poppoo = new Henkilot();
+            Henkilo hlo = new Henkilo { Etunimi = "Jack ", Sukunimi = "Sparrow ", HeTu = "987652-754Å " };
+            Henkilo hlo1 = new Henkilo { Etunimi = "James ", Sukunimi = "Bond ", HeTu = "007007-007A " };
+            Henkilo hlo2 = new Henkilo { Etunimi = "Ninjaspidar ", Sukunimi = "Kurwaspidar ", HeTu = "696969-6969 " };
+            //lisätään henkilöt poppooseen
+            poppoo.LisaaHenkilo(hlo);
+            poppoo.LisaaHenkilo(hlo1);
+            poppoo.LisaaHenkilo(hlo2);
+            foreach (Henkilo h in poppoo.HenkiloLista)
+            {
+                Console.WriteLine("{0}", h.ToString());
+            }
+            //TODO kysy käyttäjältä hetu ja haetaan sitä vastaava henkilö näytölle
+            Console.WriteLine("Syötä Hetu: ");
+
+        }
+    }
+}
